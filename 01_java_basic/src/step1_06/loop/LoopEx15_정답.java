658aa500a629c86d5c0b2bca156c71786bff5d84
yuri-note <yuri-note@LAPTOP-NI438OED>
2022-08-22 18:43:01 +0900

    re

--- 01_java_basic/src/step1_06/loop/LoopEx15_정답.java
+++ 01_java_basic/src/step1_06/loop/LoopEx15_정답.java
@@ -1,5 +1,8 @@
 package step1_06.loop;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /*
  * # 랜덤학생
  * 
@@ -17,6 +20,30 @@ public class LoopEx15_정답 {
 
 	public static void main(String[] args) {
 		
+		Random ran = new Random();
+		Scanner scan = new Scanner(System.in);
+		
+		int sum = 0;
+		int max = 0;
+		int passNum = 0;
+		int maxidx = 1;
+		for (int i = 0; i < 10; i++) {
+			int num = ran.nextInt(100) + 1; 
+			sum += num;
+			if (num >= 60) {
+				passNum++;
+			}
+			if (num > max) {
+				max = num ;
+				maxidx = i;
+			}
+		}
+		
+		int avg = sum / 10;
+		System.out.println("전교생 총점은 " + sum + "점, 평균은 " + avg + "점");
+		System.out.println("합격자 수는 " + passNum + "명이다.");
+		System.out.println("1등 학생의 번호는 " + (maxidx+1) +", 점수는 " + max + "점이다.");
+
 	}
 
 }
